cbefac1a1a093473e9f54986df6eb7a9619e883c
jay-senjaliya <jaysenjaliya0070@gmail.com>
2023-03-05 11:13:36 +0530

    Day-4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './pages/Counter';
 import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Counter />
     </div>
   );
 }
