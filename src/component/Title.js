af41380765ee0e8be1631693829c48ddc980504d
jay-senjaliya <jaysenjaliya0070@gmail.com>
2023-03-05 10:17:40 +0530

    Day-3

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Title(props) {
+    const { title, description } = props;
     return (
         <div>
-            <h1>{props.title}</h1>
-            <p>This is a pharagraph.</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react'
 import Title from '../component/Title'
 
 export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = { title: 'Title using state', description: 'this is a description using state' }
+        // this.setState(
+        // title = 'Title using state',
+        // discription = 'this is a description using state'
+        // )
+        console.log(this.state.title);
+    }
+
     render() {
         return (
             <div>
-                <Title title='Title1' />
-                <Title title='Title2' />
-                <Title title='Title3' />
+                <Title title={this.state.title} description={this.state.description} />
+                <Title title={this.state.title} description={this.state.description} />
+                <Title title={this.state.title} description={this.state.description} />
             </div>
         )
     }
